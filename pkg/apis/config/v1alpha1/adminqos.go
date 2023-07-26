c2386c53b28bbb4138954d550249f716d1b80496
周鸿斌 <zhouhongbin@bytedance.com>
2023-07-26 14:37:49 +0800

    refactor(qrm): keep naming style in line with existing field

--- pkg/apis/config/v1alpha1/adminqos.go
+++ pkg/apis/config/v1alpha1/adminqos.go
@@ -94,9 +94,9 @@ type ReclaimedResourceConfig struct {
 	// +optional
 	ReservedResourceForAllocate *v1.ResourceList `json:"reservedResourceForAllocate,omitempty"`
 
-	// ReservedResourceForReclaim is a resource reserved for reclaimed_cores pods.
+	// MinReclaimedResourceForAllocate is a resource reserved for reclaimed_cores pods.
 	// +optional
-	ReservedResourceForReclaimedCores *v1.ResourceList `json:"ReservedResourceForReclaimedCores,omitempty"`
+	MinReclaimedResourceForAllocate *v1.ResourceList `json:"minReclaimedResourceForAllocate,omitempty"`
 
 	// CPUHeadroomConfig is a configuration for cpu headroom
 	// +optional
