2c550c6d81b5b32c5ff234ad9e993ae7a94a21d9
koufeixiang <koufeixiang@bytedance.com>
2025-07-16 17:45:41 +0800

    feat(*): add L3CacheGroup topology type

--- pkg/apis/node/v1alpha1/types.go
+++ pkg/apis/node/v1alpha1/types.go
@@ -215,6 +215,9 @@ const (
 	// TopologyTypeNuma indicates numa-level topology
 	TopologyTypeNuma TopologyType = "Numa"
 
+	// TopologyTypeCacheGroup indicates cache-group-level topology
+	TopologyTypeCacheGroup TopologyType = "L3CacheGroup"
+
 	// TopologyTypeGPU indicates a zone for gpu device
 	TopologyTypeGPU TopologyType = "GPU"
 
